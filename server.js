8dc87d722a8a6d2bedcea1c233bd4c661e98b5f6
Honey Sanghani <honeysanghani98@gmail.com>
2022-03-20 01:57:31 -0400

    CRUD path added

--- server.js
+++ server.js
@@ -66,27 +66,28 @@ app.delete('/api/candidate/:id',(req,res)=>{
     });
 });
 //create a candidate
-app.post('/api/candidate',({body},res)=>{
+app.post('/api/candidate',({ body },res)=>{
     const errors=inputCheck(body,'first_name','last_name','industry_connected');
     if(errors){
         res.status(400).json({error:errors});
         return;
     }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-                    VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
+    const sql=`INSERT INTO candidates (first_name,last_name,industry_connected)
+                VALUES(?,?,?)`;
+    const params=[body.first_name,body.last_name,body.industry_connected];
+    db.query(sql,params,(err,result)=>{
+        if(err){
+            res.status(400).json({error:err.message});
             return;
         }
         res.json({
-        message: 'success',
-        data: body
+            message:'success',
+            data:body
         });
+
     });
-})
+});
+
 // response for any other request which is not found
 app.use((req,res)=>{
     res.status(404).end();
