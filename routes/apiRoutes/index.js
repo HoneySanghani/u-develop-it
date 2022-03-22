863694194998fdb62b7f29c7defeedfaf67c5dd7
Honey Sanghani <honeysanghani98@gmail.com>
2022-03-22 00:06:01 -0400

    vote counting added

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router=express.Router();
 router.use(require('./candidateRoutes'));
 router.use(require('./partiesRoutes'));
 router.use(require('./votersRoutes'));
+router.use(require('../apiRoutes/voteRoutes'));
 
 module.exports=router;
